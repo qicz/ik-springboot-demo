948de0148d1abce99c17a582d2d27b088794c141
Qicz <qiczzhu@gmail.com>
2021-01-05 20:42:59 +0800

    update

--- ik-demo-services/src/main/java/abc/kit/Kit.java
+++ ik-demo-services/src/main/java/abc/kit/Kit.java
@@ -38,62 +38,61 @@ public final class Kit {
 
             final String configPath = "config/";
             File config = new File(System.getProperty("user.dir") + "/" + configPath);
-            if (!config.exists()) {
-                if (!config.mkdir()) {
-                    return;
-                }
-                JarFile jarFile = new JarFile(source);
-                final Set<String> configFiles = new HashSet<String>(){{
-                    add(".properties");
-                    add(".yaml");
-                    add(".yml");
-                    add(".xml");
-                }};
-                for (Enumeration<? extends ZipEntry> entries = jarFile.entries(); entries.hasMoreElements(); ) {
-                    ZipEntry entry = entries.nextElement();
-                    String entryName = entry.getName();
-                    // 仅拷贝config目录或properties,yaml。
-                    boolean isConfig = false;
-                    int lastIndexOf = entryName.indexOf(configPath);
-                    String outPath = "";
-                    // has config dir
+            if (config.exists() || !config.mkdir()) {
+                return;
+            }
+
+            JarFile jarFile = new JarFile(source);
+            final Set<String> configFiles = new HashSet<String>(){{
+                add(".properties");
+                add(".yaml");
+                add(".yml");
+                add(".xml");
+            }};
+            for (Enumeration<? extends ZipEntry> entries = jarFile.entries(); entries.hasMoreElements(); ) {
+                ZipEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                // 仅拷贝config目录或properties,yaml。
+                boolean isConfig = false;
+                int lastIndexOf = entryName.indexOf(configPath);
+                String outPath = "";
+                // has config dir
+                if (lastIndexOf != -1) {
+                    outPath = entryName.substring(lastIndexOf);
+                    isConfig = true;
+                } else {
+                    lastIndexOf = entryName.lastIndexOf(".");
                     if (lastIndexOf != -1) {
-                        outPath = entryName.substring(lastIndexOf);
-                        isConfig = true;
-                    } else {
-                        lastIndexOf = entryName.lastIndexOf(".");
-                        if (lastIndexOf != -1) {
-                            String file = entryName.substring(entryName.lastIndexOf("/") + 1);
-                            boolean pomFile = "pom.properties".equals(file) || "pom.xml".equals(file);
-                            isConfig = configFiles.contains(entryName.substring(lastIndexOf)) && !pomFile;
-                            if (isConfig) {
-                                outPath = String.join("", configPath, file);
-                            }
+                        String file = entryName.substring(entryName.lastIndexOf("/") + 1);
+                        boolean pomFile = "pom.properties".equals(file) || "pom.xml".equals(file);
+                        isConfig = configFiles.contains(entryName.substring(lastIndexOf)) && !pomFile;
+                        if (isConfig) {
+                            outPath = String.join("", configPath, file);
                         }
                     }
-
-                    if (!isConfig) {
-                        continue;
-                    }
-                    InputStream jarFileInputStream = jarFile.getInputStream(entry);
-                    File currentFile = new File(outPath.substring(0, outPath.lastIndexOf('/')));;
-                    if (!currentFile.exists() && !currentFile.mkdirs()) {
-                        continue;
-                    }
-                    // 判断文件全路径是否为文件夹,如果是上面已经上传,不需要解压
-                    if (new File(outPath).isDirectory()) {
-                        continue;
-                    }
-
-                    FileOutputStream out = new FileOutputStream(outPath);
-                    byte[] bytes = new byte[1024];
-                    int len;
-                    while ((len = jarFileInputStream.read(bytes)) > 0) {
-                        out.write(bytes, 0, len);
-                    }
-                    jarFileInputStream.close();
-                    out.close();
                 }
+
+                if (!isConfig) {
+                    continue;
+                }
+                InputStream jarFileInputStream = jarFile.getInputStream(entry);
+                File currentFile = new File(outPath.substring(0, outPath.lastIndexOf('/')));;
+                if (!currentFile.exists() && !currentFile.mkdirs()) {
+                    continue;
+                }
+                // 判断文件全路径是否为文件夹,如果是上面已经上传,不需要解压
+                if (new File(outPath).isDirectory()) {
+                    continue;
+                }
+
+                FileOutputStream out = new FileOutputStream(outPath);
+                byte[] bytes = new byte[1024];
+                int len;
+                while ((len = jarFileInputStream.read(bytes)) > 0) {
+                    out.write(bytes, 0, len);
+                }
+                jarFileInputStream.close();
+                out.close();
             }
         }
     }
